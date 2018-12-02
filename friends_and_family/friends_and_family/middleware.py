8dc910b481c73f33dab88984505fc549ff9f43f5
Vubon Roy <vubon.roy@gmail.com>
2018-12-03 01:07:29 +0600

    added token validation method

--- friends_and_family/friends_and_family/middleware.py
+++ friends_and_family/friends_and_family/middleware.py
@@ -7,33 +7,34 @@ from applibs.auth_service import AuthService
 auth = AuthService()
 
 
+class TokenValidationTwo(MiddlewareMixin):
+    def process_request(self, request):
+        if "HTTP_AUTHORIZATION" not in request.META:
+            return HttpResponseBadRequest("You have no permission")
+
+    def process_response(self, request, response):
+        token = request.META.get('HTTP_AUTHORIZATION', None)
+
+        if not token:
+            return HttpResponseBadRequest(json.dumps({"error_msg": "Invalid Token"}))
+
+        res, status_code = auth.token_validation({"user_token": token})
+
+        if 500 <= status_code <= 599:
+            response['error_msg'] = res
+            response['status_code'] = status_code
+
+        if 400 <= status_code <= 499:
+            response['error_msg'] = res
+            response['status_code'] = status_code
+
+        return response
+
+
 class TokenValidation:
     """
         Token Validation Middleware
     """
-
-    # def process_request(self, request):
-    #     if "HTTP_AUTHORIZATION" not in request.META:
-    #         return HttpResponseBadRequest("You have no permission")
-    #
-    # def process_response(self, request, response):
-    #     token = request.META.get('HTTP_AUTHORIZATION', None)
-    #
-    #     if not token:
-    #         return HttpResponseBadRequest(json.dumps({"error_msg": "Invalid Token"}))
-    #
-    #     res, status_code = auth.token_validation({"user_token": token})
-    #
-    #     if 500 <= status_code <= 599:
-    #         response['error_msg'] = res
-    #         response['status_code'] = status_code
-    #
-    #     if 400 <= status_code <= 499:
-    #         response['error_msg'] = res
-    #         response['status_code'] = status_code
-    #
-    #     return response
-
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
